c413a6eff2b4bb78b6c7f7059e12ccd43d7bbb3f
Seokhyun Bae <bus1029@gmail.com>
2021-05-28 15:35:29 +0900

    feat: 스프링 핵심 원리 최초 커밋
    # |<---- Using a Maximum Of 50 Characters ---->|
    #
    # Explain why this change is being made
    # |<----  Try To Limit Each Line to a Maximum Of 72 Characters  ---->|
    #
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    #
    # --- COMMIT END ---
    # Type can be
    #   feat   (new feature)
    #   fix   (bug fix)
    #   refactor (refactoring production code)
    #   style  (formatting, missing semi colons, etc; no code change)
    #   docs   (changes to documentation)
    #   test   (adding or refactoring tests; no production code change)
    #   chore  (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #   - Capitalize the subject line
    #   - Use the imperative mood in the subject line
    #   - Do not end the subject line with a period
    #   - Separate subject from body with a blank line
    #   - Use the body to explain what and why vs. how
    #   - Can use multiple lines with “-” for bullet points in body
    # --------------------
    # For updated template, visit:
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50
    # Licence CC

---
+++ src/test/kotlin/hello/springCore/SpringCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.springCore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
