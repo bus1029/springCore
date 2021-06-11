4ed643ffb450015a96d333c7908107683c16a36b
Seokhyun Bae <bus1029@gmail.com>
2021-06-11 19:48:34 +0900

    feat: Provider를 사용한 프로토타입 빈 주입
    - Provider와 ObjectProvider를 사용하여 필요할 때마다 프로토타입 빈을 주입받는
    코드 작성

--- src/test/kotlin/hello/springCore/scope/SingletonWithPrototype.kt
+++ src/test/kotlin/hello/springCore/scope/SingletonWithPrototype.kt
@@ -2,9 +2,11 @@ package hello.springCore.scope
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Scope
+import javax.inject.Provider
 
 class SingletonWithPrototype {
   @Test
@@ -16,13 +18,23 @@ class SingletonWithPrototype {
 
     val clientBean2 = ac.getBean(ClientBean::class.java)
     val logic2 = clientBean2.logic()
-    Assertions.assertThat(logic2).isEqualTo(2)
+    Assertions.assertThat(logic2).isEqualTo(1)
   }
 
   @Scope("singleton")
-  class ClientBean @Autowired constructor(private val prototypeBean: PrototypeBean) {
-    // prototypeBean 은 생성 시점에 주입되어 버림
+  class ClientBean @Autowired constructor(private val prototypeBeanProvider: ObjectProvider<PrototypeBean>) {
     fun logic(): Int {
+      // 필요할 때마다 스프링 컨테이너에 요청
+      val prototypeBean = prototypeBeanProvider.getObject()
+      return prototypeBean.addCount()
+    }
+  }
+
+  @Scope("singleton")
+  class ClientBean2 @Autowired constructor(private val prototypeBeanProvider: Provider<PrototypeBean>) {
+    fun logic(): Int {
+      // 필요할 때마다 스프링 컨테이너에 요청
+      val prototypeBean = prototypeBeanProvider.get()
       return prototypeBean.addCount()
     }
   }
